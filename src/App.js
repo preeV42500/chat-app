6bf6fc2f35f8584c058c1365c4f2d6628fe2016b
Preeti Viswanathan <vis.preeti@gmail.com>
2018-05-06 10:51:37 -0700

    create List component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const messages = [
+  {
+    id: 1,
+    text: 'hi',
+    author: 'Ben',
+    channel_id: 1
+  },
+  {
+    id: 2,
+    text: 'hi to you too',
+    author: 'Jen',
+    channel_id: 1
+  },
+  {
+    id: 3,
+    text: 'hi from another channel',
+    author: 'Meg',
+    channel_id: 2
+  },
+  {
+    id: 4,
+    text: 'hi to you too from another channel',
+    author: 'Jeff',
+    channel_id: 2
+  }
+];
 
 const Message = ({author, text}) => (
   <div className="Message">
     <div className="Message-author">{author}</div>
     <div className="Message-text">{text}</div>
   </div>
-)
+);
 
+const List = ({messages}) => (
+  <div className="MessagePane-List">
+    { messages.map(({id, author, text}) => <Message key={id} author={author} text={text} />) }
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <Message author="Ben" text="hi" />
-        <Message author="Jen" text="hi to you too" />
-
+        <List messages={messages} />
       </div>
     );
   }
