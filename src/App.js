751ee17de86ac845fb5bb68315683ff883a12d08
Preeti Viswanathan <vis.preeti@gmail.com>
2018-05-06 10:21:14 -0700

    create Message functional component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const Message = ({author, text}) => (
+  <div className="Message">
+    <div className="Message-author">{author}</div>
+    <div className="Message-text">{text}</div>
+  </div>
+)
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Message author="Ben" text="hi" />
+        <Message author="Jen" text="hi to you too" />
+
       </div>
     );
   }
